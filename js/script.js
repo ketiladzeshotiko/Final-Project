5e54b0d6e61e4c8b7569b8d3e889e7c5a834f760
ketiladzeshotiko <ketiladzeshotiko10@gmail.com>
2025-05-25 16:40:13 +0400

    add burgerBar /js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+let burgerEL = document.getElementById('burgerBar');
+let navEl = document.getElementById('navBar');
+
+burgerEL.addEventListener('click', function() {
+    navEl.classList.toggle("activeNav")
+});
 
